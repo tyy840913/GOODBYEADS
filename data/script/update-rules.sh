f78a50664f17d8a3b47d953fa602a4e35296497f
tyy840913 <44389998+tyy840913@users.noreply.github.com>
2024-06-21 16:28:19 +0800

    Update update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -19,7 +19,6 @@ rules=(
   "http://rssv.cn/adguard/api.php?type=host" #晴雅HOST规则
   "https://cdn.jsdelivr.net/gh/banbendalao/ADgk@master/ADgk.txt" #ADGK手机规则
   "https://cdn.jsdelivr.net/gh/Silentely/AdBlock-Acceleration/ds_hosts.txt"  #大圣净化
-  "https://perflyst.github.io/PiHoleBlocklist/SmartTV-AGH.txt" #Tv规则  
   "https://easylist-downloads.adblockplus.org/easylistchina.txt" #EasyList China
   "https://easylist-downloads.adblockplus.org/easyprivacy.txt" #EasyPrivacy隐私保护规则
   "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt" #秋风规则
