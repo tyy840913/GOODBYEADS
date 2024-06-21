61a61a8ddcca763dbac65b6bce339a14006472f4
tyy840913 <44389998+tyy840913@users.noreply.github.com>
2024-06-21 12:30:16 +0800

    Update title.py

--- data/python/title.py
+++ data/python/title.py
@@ -20,8 +20,8 @@ for file_path in file_list:
 
     # 在文件顶部插入内容
     new_content = f"[Adblock Plus 2.0]\n" \
-                  f"! Title: GOODBYEADS\n" \
-                  f"! Homepage: https://github.com/8680/GOODBYEADS\n" \
+                  f"! Title: NOT-AD\n" \
+                  f"! Homepage: https://github.com/tyy840913/NOT-AD\n" \
                   f"! Expires: 12 Hours\n" \
                   f"! Version: {beijing_time}（北京时间）\n" \
                   f"! Description: 适用于AdGuard的去广告规则，合并优质上游规则并去重整理排列\n" \
